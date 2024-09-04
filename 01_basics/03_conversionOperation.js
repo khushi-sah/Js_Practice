41c27c5cf0ad669a9853a91bb1a7f42627bf2d7b
khushi-sah <khushikumariit03@gmail.com>
2024-09-04 12:28:35 +0000

    Comparison and Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,56 @@
+let score="khushi"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber= Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" =>33
+//"33abc" => NaN (NOT A NUMBER)
+//true => 1; false=> 0
+
+let isLoggedIn ="khushi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 => true; 0=> false
+//"" =>false
+//"khushi" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(someNumber);
+console.log(typeof stringNumber);
+
+//***************OPERATIONS*************** */
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2/2);
+//console.log(2%3);
+
+let str1="hello"
+let str2= "khushi"
+
+let str3= str1 +str2
+console.log(str3);
+
+//console.log("1"+ 2); o/p==12
+//console.log(1+ "2"); o/p==12
+//console.log("1"+ 2+2); o/p==122
+//console.log(1+ 2 +"2"); o/p==32
+
+//console.log((3 +4) * 5 % 3); 
+
+let gameCounter= 100
+gameCounter++;
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,25 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2==1);
+//console.log(2!=1);
+
+
+/* not used in code
+//console.log("2">1); o/p=true
+//console.log("02"=1); o/p=true
+
+//console.log(null >0); o/p=false
+//console.log(null ==0); o/p=false
+//console.log(null >= 0); o/p=true
+
+//console.log(undefined >0); o/p=false
+//console.log(undefined == 0); o/p=false
+//console.log(undefined < 0); o/p=false
+
+
+*/
+// ===
+console.log("2" === 2);
+
+
