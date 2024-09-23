234b118d0e8302e65ddc1f249619206f8f768ee1
khushi-sah <khushikumariit03@gmail.com>
2024-09-23 12:34:21 +0000

    CALL Stack

--- 06_IIFE.js
+++ 06_IIFE.js
@@ -11,3 +11,41 @@
 ((name) => {
     console.log(`DB Connectd Two ${name}`);
 } ) ('Khushi')
+
+
+//======================================
+//======================================
+// JAVAScript Execeutiopn Context
+//=============================
+//Global Execution Context
+//Function Execution Context
+//EVAL Execution Context
+//================================
+//{} = memory creation phase
+
+// 1. Global Execution  : this
+
+// 2. Memory Phase
+// suppose this is the function we define
+//let val1 =10
+// let val2 = 5
+// function addNum (num1, num2){
+//     let total = num1 +num2
+//     return total
+// }
+// let result1 = addNum(val1, val2)
+// let result2 =-addNum(10,2)
+
+// memory creation :=======
+//val1 = undefined
+//val2 = undefined
+//addNum = defination
+//result1 = undefined
+//result2 = undefined
+
+// 3. execution Phase
+//val1 = 10
+//val2 = 2
+//addNum = defination
+//result1 = undefined
+//result2 = undefined
