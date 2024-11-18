89eeb551dbbed8648110c01f9c9a5ed042b6480b
khushi-sah <khushikumariit03@gmail.com>
2024-11-18 06:31:11 +0000

    Reduce

--- 06_Loop_Iterations/eight.js
+++ 06_Loop_Iterations/eight.js
@@ -1,4 +1,35 @@
 //reduce()
+const myNums = [1,2,3]
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+
+console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "Python course",
+        price: 999
+    },
+    {
+        itemName: "Mobile Dev course",
+        price: 5999
+    },
+    {
+        itemName: "Data Science course",
+        price: 12999
+    }
+  ]
+const priceToPay = shoppingCart.reduce( (acc, item) => acc + item.price,0)
+
+console.log(priceToPay);
 
 
 
